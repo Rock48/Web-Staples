3b31633705edd396572fabf411017dac49553ba0
Rock48 <cquiglz@gmail.com>
2014-12-14 14:31:29 -0500

    Forgot to commit this last night-- mobile version is a bit more friendly

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
+require 'certified'
 require 'sinatra'
+require 'open-uri'
+require 'open_uri_redirections'
 
 set :bind, '0.0.0.0'
+set :views, Proc.new { File.join(root, 'public') }
 
 get '/' do
-    send_file File.join(settings.public_folder, 'index.html')
+    #@time = open('http://tv.csapp.westport.k12.ct.us/api/time/now', :allow_redirections => :safe).read
+    erb :index
 end
