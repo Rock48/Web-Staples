9accea347622d32b3d83821dfbd084473f64ec75
Rock48 <cquiglz@gmail.com>
2014-12-15 15:32:46 -0500

    Announcements and server time sync- OpenSSL is killing me right now

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
-require 'certified'
 require 'sinatra'
 require 'open-uri'
-require 'open_uri_redirections'
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 set :bind, '0.0.0.0'
 set :views, Proc.new { File.join(root, 'public') }
 
 get '/' do
-    #@time = open('http://tv.csapp.westport.k12.ct.us/api/time/now', :allow_redirections => :safe).read
+    @time = open('https://tv.csapp.westport.k12.ct.us/api/time/now').read
+    @announcements = open('https://tv.csapp.westport.k12.ct.us/api/announcement/today').read
     erb :index
 end
