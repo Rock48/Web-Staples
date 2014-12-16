fd53f088e665dda940cc229cf4e56b524cfe37bb
Rock48 <cquiglz@gmail.com>
2014-12-16 17:00:30 -0500

    Finally figured out the bug with OpenSSL, you must grab the GoDaddy cert g2 bundle on their website for it to work.

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
 require 'open-uri'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'json'
 
 set :bind, '0.0.0.0'
 set :views, Proc.new { File.join(root, 'public') }
 
+CRT_FILE = File.join('C:', 'Ruby193', 'gd_bundle-g2.crt').to_s
+
 get '/' do
-    @time = open('https://tv.csapp.westport.k12.ct.us/api/time/now').read
-    @announcements = open('https://tv.csapp.westport.k12.ct.us/api/announcement/today').read
+    @time = open('https://tv.csapp.westport.k12.ct.us/api/time/now', :ssl_ca_cert => CRT_FILE).read
+    @announcements = open('https://tv.csapp.westport.k12.ct.us/api/announcement/today', :ssl_ca_cert => CRT_FILE).read
+    @schedule = open('https://tv.csapp.westport.k12.ct.us/api/schedule/today', :ssl_ca_cert => CRT_FILE).read
+
+    @schedule_len = JSON.parse!(@schedule).length
+
+
     erb :index
 end
